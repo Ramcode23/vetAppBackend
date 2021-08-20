a755ce125d71eb3e0fc9038dd396b31a53e8cfc3
Raphy Mejia <moyeto3@gmail.com>
2021-08-20 13:05:20 -0400

    initial respository

---
+++ Entities/DataContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using vetappback.Entities;
+
+namespace vetappback.Models {
+    public class DataContext : DbContext {
+        public DataContext() { }
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+
+
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<PetType> PetTypes { get; set; }
+
+    	public DbSet<Pet> Pets { get; set; }
+
+    	public DbSet<ServiceType> ServiceTypes { get; set; }
+
+    	public DbSet<History> Histories { get; set; }
+
+    	public DbSet<Agenda> Agendas { get; set; }
+
+
+    }
+}
